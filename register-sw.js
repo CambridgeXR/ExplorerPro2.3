3cfe5bd69a15b8c10fe4ee70e6fceb5eaa0e54f5
CambridgeXR <chris.macdonald@lucy.cam.ac.uk>
2025-11-24 12:19:18 +0000

    Update register-sw.js

--- register-sw.js
+++ register-sw.js
@@ -1,8 +1,8 @@
-// register-sw.js for VR Explorer Pro
+// register-sw.js for VR Explorer Pro v2.3
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', async () => {
     try {
-      const reg = await navigator.serviceWorker.register('./sw.js', { scope: '/Explorer6/' });
+      const reg = await navigator.serviceWorker.register('./sw.js?v=23', { scope: '/ExplorerPro2.3/' });
       console.log('VR Explorer Pro: serviceWorker registered.', reg);
       reg.addEventListener('updatefound', () => {
         const newWorker = reg.installing;
